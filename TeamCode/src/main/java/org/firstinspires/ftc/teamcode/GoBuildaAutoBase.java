9c20762be552e301aa8156533192470f3b725be8
Benjamin4476 <bendenotterversteeg@gmail.com>
2021-09-14 16:32:11 -0400

    Worked on odometry and kinematics

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBuildaAutoBase.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.MecanumNavigation;
+
+@Autonomous(name = "Gobuilda Auto Base")
+public class GoBuildaAutoBase extends LinearOpMode {
+    private double x = 0;
+    @Override
+    public void runOpMode(){
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumNavigation.java
@@ -1,11 +1,6 @@
-/*
-Op mode to use kinematics and odometry to drive the robot to a target location.
-More information about FTCLib can be found at https://docs.ftclib.org/ftclib/
- */
-
 package org.firstinspires.ftc.teamcode;
 
-import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.geometry.Translation2d;
@@ -16,29 +11,31 @@ import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveKinematics
 import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveOdometry;
 import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveWheelSpeeds;
 import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
-@TeleOp(name="GoBuilda Drive Odometry Java")
-public class GoBuildaOdometry extends LinearOpMode {
+public class MecanumNavigation extends SubsystemBase {
     private ElapsedTime runtime = new ElapsedTime();
 
-    private MotorEx frontLeftMotor;
-    private MotorEx frontRightMotor;
-    private MotorEx backLeftMotor;
-    private MotorEx backRightMotor;
+    private final MotorEx frontLeftMotor;
+    private final MotorEx frontRightMotor;
+    private final MotorEx backLeftMotor;
+    private final MotorEx backRightMotor;
 
-    private BNO055IMU imu;
+    private final BNO055IMU imu;
 
-    @Override
-    public void runOpMode() {
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
+    public MecanumNavigation(MotorEx frontLeftMotor, MotorEx frontRightMotor, MotorEx backLeftMotor, MotorEx backRightMotor, BNO055IMU imu, double xStart, double yStart, double omegaStart){
+        this.frontLeftMotor = frontLeftMotor;
+        this.frontRightMotor = frontRightMotor;
+        this.backLeftMotor = backLeftMotor;
+        this.backRightMotor = backRightMotor;
+        this.imu = imu;
+    }
 
+    public void initialize(){
         BNO055IMU.Parameters imuParameters;
 
         // Create new IMU Parameters object.
@@ -51,13 +48,6 @@ public class GoBuildaOdometry extends LinearOpMode {
         imuParameters.loggingEnabled = false;
         // Initialize IMU.
         imu.initialize(imuParameters);
-
-
-        // Assign motor variables to corresponding hardware
-        frontLeftMotor = new MotorEx(hardwareMap, "FL", Motor.GoBILDA.RPM_312);
-        frontRightMotor = new MotorEx(hardwareMap, "FR", Motor.GoBILDA.RPM_312);
-        backLeftMotor = new MotorEx(hardwareMap, "BL", Motor.GoBILDA.RPM_312);
-        backRightMotor = new MotorEx(hardwareMap, "BR", Motor.GoBILDA.RPM_312);
 
         // Set motors to run at a specified velocity
         frontLeftMotor.setRunMode(Motor.RunMode.VelocityControl);
@@ -82,11 +72,13 @@ public class GoBuildaOdometry extends LinearOpMode {
         backRightMotor.resetEncoder();
 
         // Set how far the wheels move in millimeters per encoder pulse
-        frontLeftMotor.setDistancePerPulse(0.557);
-        frontRightMotor.setDistancePerPulse(0.557);
-        backLeftMotor.setDistancePerPulse(0.557);
-        backRightMotor.setDistancePerPulse(0.557);
+        frontLeftMotor.setDistancePerPulse(0.59);
+        frontRightMotor.setDistancePerPulse(0.59);
+        backLeftMotor.setDistancePerPulse(0.59);
+        backRightMotor.setDistancePerPulse(0.59);
+    }
 
+    public void initializeDrivetrain(double xStart, double yStart, double omegaStart){
         // Setup the odometry system
         // TODO: change all numbers from placeholder values to the values on the robot
         // Set the locations of the wheels on the robot
@@ -97,31 +89,19 @@ public class GoBuildaOdometry extends LinearOpMode {
         // Create a mecanum kinematics object from the wheel locations
         MecanumDriveKinematics kinematics = new MecanumDriveKinematics(frontLeftLocation,frontRightLocation,backLeftLocation,backRightLocation);
         // Create a chassis speeds object relative to the field - format is (desired speed relative to one wall, desired speed relative to second wall, desired rotation speed in radians, current angle)
-        ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(1.0,1.0,1.0,Rotation2d.fromDegrees(45.0));
+        ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(1.0,1.0,1.0, Rotation2d.fromDegrees(45.0));
         // Create an object to hold the angle of the robot as reported by the IMU
         Rotation2d gyroAngle = Rotation2d.fromDegrees(imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle);
 
         MecanumDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(speeds);
 
-        double frontLeftSpeed = wheelSpeeds.frontLeftMetersPerSecond;
-        double frontRightSpeed = wheelSpeeds.frontRightMetersPerSecond;
-        double backLeftSpeed = wheelSpeeds.rearLeftMetersPerSecond;
-        double backRightSpeed = wheelSpeeds.rearRightMetersPerSecond;
+        MecanumDriveOdometry odometry = new MecanumDriveOdometry(kinematics, gyroAngle, new Pose2d(xStart, yStart, new Rotation2d(omegaStart)));
+    }
 
-        // Create an odometry object - format is (kinematics object, gyro heading, start position on field)
-        MecanumDriveOdometry odometry = new MecanumDriveOdometry(kinematics, gyroAngle, new Pose2d(5, 5, new Rotation2d()));
+    @Override
+    public void periodic(){
+        MecanumDriveWheelSpeeds wheelSpeeds = new MecanumDriveWheelSpeeds(frontLeftMotor.getVelocity(), frontRightMotor.getVelocity(), backLeftMotor.getVelocity(), backRightMotor.getVelocity());
+        Rotation2d gyroAngle = Rotation2d.fromDegrees(imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle);
 
-        // Update telemetry
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        // Wait for game to start
-        waitForStart();
-        runtime.reset();
-
-        while (opModeIsActive()) {
-
-            telemetry.update();
-        }
     }
 }
